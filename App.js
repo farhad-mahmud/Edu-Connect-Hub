cf514b54beeb3de2e3e8d16ed8e88165be81c859
Shahriar Kobir Moon <22235103663@cse.bubt.edu.bd>
2025-08-03 14:53:19 +0600

    first com

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, SafeAreaView } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+ let x = 1;
+      console.log("app executed"); 
+      return (
+    <SafeAreaView style={styles.container}>
+      <Text>Welcome to React Native!</Text>
+      <Button
+        title="Click Me"
+        onPress={() => {
+          x++;
+          window.alert("Button clicked ${x}",);
+        }}
+      />
+      
+      </SafeAreaView>
+      
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'cyan',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
